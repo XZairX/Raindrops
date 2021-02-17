866c14bb893eb8a91bb17b79b93a21389e40f62d
Zair <michaelxzr@gmail.com>
2021-02-17 16:20:24 +0000

    Add negative [TestCase]s to RaindropsTests.cs

--- Raindrops/RaindropsTests/RaindropsTests.cs
+++ Raindrops/RaindropsTests/RaindropsTests.cs
@@ -8,6 +8,9 @@ namespace RaindropsTests
     {
         private readonly Raindrops _sut = new Raindrops();
 
+        [TestCase(-34)]
+        [TestCase(-8)]
+        [TestCase(-1)]
         [TestCase(1)]
         [TestCase(8)]
         [TestCase(34)]
@@ -18,6 +21,9 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo(number.ToString()));
         }
 
+        [TestCase(-27)]
+        [TestCase(-9)]
+        [TestCase(-3)]
         [TestCase(3)]
         [TestCase(9)]
         [TestCase(27)]
@@ -28,6 +34,9 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("Pling"));
         }
 
+        [TestCase(-25)]
+        [TestCase(-10)]
+        [TestCase(-5)]
         [TestCase(5)]
         [TestCase(10)]
         [TestCase(25)]
@@ -38,6 +47,9 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("Plang"));
         }
 
+        [TestCase(-28)]
+        [TestCase(-14)]
+        [TestCase(-7)]
         [TestCase(7)]
         [TestCase(14)]
         [TestCase(28)]
@@ -48,6 +60,8 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("Plong"));
         }
 
+        [TestCase(-30)]
+        [TestCase(-15)]
         [TestCase(15)]
         [TestCase(30)]
         public void PlingPlangPlong_NumberHasAFactorOf3And5_ReturnsPlingPlang(int number)
@@ -57,6 +71,8 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("PlingPlang"));
         }
 
+        [TestCase(-63)]
+        [TestCase(-21)]
         [TestCase(21)]
         [TestCase(63)]
         public void PlingPlangPlong_NumberHasAFactorOf3And7_ReturnsPlingPlong(int number)
@@ -66,6 +82,8 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("PlingPlong"));
         }
 
+        [TestCase(-70)]
+        [TestCase(-35)]
         [TestCase(35)]
         [TestCase(70)]
         public void PlingPlangPlong_NumberHasAFactorOf5And7_ReturnsPlangPlong(int number)
@@ -75,6 +93,8 @@ namespace RaindropsTests
             Assert.That(result, Is.EqualTo("PlangPlong"));
         }
 
+        [TestCase(-210)]
+        [TestCase(-105)]
         [TestCase(105)]
         [TestCase(210)]
         public void PlingPlangPlong_NumberHasAFactorOf3And5And7_ReturnsPlingPlangPlong(int number)
